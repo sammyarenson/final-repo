0ad16714beeaddbfc2aaa6efe3c40e056f0c722b
sammyarenson <9sarenson@stpatrick.org>
2016-12-13 13:22:27 -0600

    day1

---
+++ cart.js
@@ -0,0 +1,14 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(itemList,itemQuantity){
+    this.itemList=itemList;  // An array that contains pre-made item-objects.
+    this.itemQuantity=itemQuantity; // An array that stores how many of each corresponding item you have in the cart.
+  }
+addItem(itemList,itemQuantity){
+  this.itemList = i;
+  this.itemQuantity = q;
+}
+
+  //Type the instance functions below this comment.
+
+}
---
+++ item.js
@@ -0,0 +1,12 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor (n,p,s){
+    this.name=n;
+    this.price=p;
+    this.shipping=s;
+  }
+}
+//Create your three test items below this comment.
+this.Tree = new Item("Huynh", 999999999, 49.99); 
+this.Trash = new Item ("AMD Products",1, 0);
+this.OtherTrash = new Item ("Consoles", 399, 0);
