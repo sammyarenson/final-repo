0b6d8a42f897a5f358e41ffb7673f4437c1baa5e
sammyarenson <9sarenson@stpatrick.org>
2016-12-13 13:57:36 -0600

    day2

--- cart.js
+++ cart.js
@@ -4,11 +4,14 @@ class Cart{
     this.itemList=itemList;  // An array that contains pre-made item-objects.
     this.itemQuantity=itemQuantity; // An array that stores how many of each corresponding item you have in the cart.
   }
-addItem(itemList,itemQuantity){
-  this.itemList = i;
-  this.itemQuantity = q;
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
 }
-
+totalCart(){
+  var total= t;
+  for(item=i;i<=itemList.l)
+}
   //Type the instance functions below this comment.
 
 }
