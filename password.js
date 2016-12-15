0101352d1ec89b435876b409102c0f674be658b5
sammyarenson <9sarenson@stpatrick.org>
2016-12-15 13:54:38 -0600

    DAy4

--- password.js
+++ password.js
@@ -1,11 +1,25 @@
 class Password{
   //The first part of the class.
-
-
-
+  constructor(pub,priv){
+    this.publickey=pub; // publickey is a string of any kind of symbol or character.
+    this.privatekey=priv; // privatekey is a secret string of numbers "xxxx-xxxx-xxxx"
+  }
   //Instance functions below this comment.
-
-
-
+  validPublicKey(){
+    if(this.public.length>8 && this.length<=25){
+      return true;
+    }
+  else{
+    return false;
+  }
+  }
+  validPrivateKey(){
+    if(this.privatekey.chat)([4]&&[9]=="x")){
+      return true;
+    }
+  }
+  else{
+    return false;
+  }
   //Static function below this comment.
 }
