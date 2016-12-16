f57522fac0855d51225f39f8709e4af3d22722f8
sammyarenson <9sarenson@stpatrick.org>
2016-12-16 13:57:42 -0600

    Final day (5)

--- password.js
+++ password.js
@@ -6,20 +6,33 @@ class Password{
   }
   //Instance functions below this comment.
   validPublicKey(){
-    if(this.public.length>8 && this.length<=25){
+    if(this.public.length>8 && this.length<=25){ //Will conditionally check to see if the string is between 8 and 25 characters long inclusively.
       return true;
     }
-  else{
-    return false;
-  }
+    else{
+      return false;
+    }
   }
   validPrivateKey(){
-    if(this.privatekey.chat)([4]&&[9]=="x")){
-      return true;
+    if(this.privatekey.charAt)([4]&&[9]=="x")){ //conditionally check if the dashes are in the correct location
+      return true; //check to see if the three groups of four characters are actually numbers
     }
   }
   else{
-    return false;
+    return false; //privatekey passes both criteria, then return true, otherwise return false
   }
   //Static function below this comment.
+  static makePrivateKey(){
+    let key = [];
+    let limit = 14;
+    let group1="";
+    let group2="";
+    let group3="";
+    for(c=0; c<4;c++){ //builds random 12 digit number with two dashes
+      group1 += Math.floor(Math.random()*10);
+      group2 += Math.floor(Math.random()*10);
+      group3 += Math.floor(Math.random()*10);
+    }
+    return group1 + "-" + group2 + "-" + group3;
+  }
 }
